08132384d2ae7c1488117343d107063106ec799a
vincentd132 <vincentd103@nycstudents.net>
2025-03-10 13:59:19 -0400

    w

---
+++ opendata/src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import Card from "@/components/DataCard.vue";
+import { ref, mounted } from "vue";
+const data = ref{[]}
+async function
+</script>
+
+<style lang="scss" scoped></style>
