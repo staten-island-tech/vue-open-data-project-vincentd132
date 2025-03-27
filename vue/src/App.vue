1d30e7b1e9093b90a0054ba3699ca5305036709e
vincentd132 <vincentd103@nycstudents.net>
2025-03-27 11:43:00 -0400

    w

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -13,3 +13,25 @@ export default {
   },
 };
 </script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+#app {
+  width: 100%;
+}
+
+.home-container {
+  width: 100%;
+}
+</style>
